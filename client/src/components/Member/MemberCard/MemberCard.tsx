2cedfafadb877835d01d998b4c9ff1459dba013d
AngryBacteria <AngryBacteria@protonmail.com>
2025-03-12 20:41:38 +0100

    new league button in layout

--- client/src/components/Member/MemberCard/MemberCard.tsx
+++ client/src/components/Member/MemberCard/MemberCard.tsx
@@ -1,4 +1,5 @@
 import { Card, Flex, Image, Text, Title } from "@mantine/core";
+import { Link } from "@tanstack/react-router";
 import { motion } from "motion/react";
 import type { Outputs } from "../../../utils/trcp.ts";
 import styles from "./MemberCard.module.css";
@@ -8,6 +9,8 @@ interface Props {
 }
 
 export function MemberCard({ member }: Props) {
+	const memberPuuid = member.leagueSummoners[0]?.puuid;
+
 	return (
 		<motion.div
 			initial={{ opacity: 0, scale: 0.9 }}
@@ -23,24 +26,46 @@ export function MemberCard({ member }: Props) {
 			>
 				<Flex
 					direction={{ base: "column", md: "row" }}
-					gap={"md"}
+					justify={{ base: "center", md: "space-between" }}
 					align={"center"}
-					justify={{ base: "center", md: "flex-start" }}
-					wrap={"wrap"}
 				>
-					<Image
-						src={`https://cnap.ch/static${member.profilePictureURL}`}
-						fallbackSrc={"https://cnap.ch/static/profilePictures/unknown.png"}
-						className={styles.memberImage}
-					/>
-					<Flex direction={"column"}>
-						<Title order={2} ta={{ base: "center", md: "left" }}>
-							{member.gameName}
-						</Title>
-						{member.punchline && (
-							<Text c={"dimmed"} ta={{ base: "center", md: "left" }}>
-								{member.punchline}
-							</Text>
+					<Flex
+						direction={{ base: "column", md: "row" }}
+						gap={"md"}
+						align={"center"}
+						justify={{ base: "center", md: "flex-start" }}
+						wrap={"wrap"}
+					>
+						<Image
+							src={`https://cnap.ch/static${member.profilePictureURL}`}
+							fallbackSrc={"https://cnap.ch/static/profilePictures/unknown.png"}
+							className={styles.memberImage}
+						/>
+						<Flex direction={"column"}>
+							<Title order={2} ta={{ base: "center", md: "left" }}>
+								{member.gameName}
+							</Title>
+							{member.punchline && (
+								<Text c={"dimmed"} ta={{ base: "center", md: "left" }}>
+									{member.punchline}
+								</Text>
+							)}
+						</Flex>
+					</Flex>
+
+					<Flex
+						direction={{ base: "row", md: "column" }}
+						gap={"md"}
+						align={"center"}
+						justify={{ base: "center", md: "flex-end" }}
+					>
+						{memberPuuid && (
+							<Link
+								to={"/summoners/$summonerPuuid"}
+								params={{ summonerPuuid: memberPuuid }}
+							>
+								<Image src={"./lol_logo.svg"} h={50} w={50} />
+							</Link>
 						)}
 					</Flex>
 				</Flex>
