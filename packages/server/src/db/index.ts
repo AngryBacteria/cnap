e30e8fa9b39da32b63af63c5f1c9d4ce166b752c
AngryBacteria <AngryBacteria@protonmail.com>
2025-05-23 11:35:51 +0200

    matches relations

--- packages/server/src/db/index.ts
+++ packages/server/src/db/index.ts
@@ -1,6 +1,8 @@
 import "dotenv/config";
 import { drizzle } from "drizzle-orm/node-postgres";
-import { GameDataTask } from "../tasks/GameDataTask.js";
+import logger from "../helpers/Logger.js";
+import riotHelper from "../helpers/RiotHelper.js";
+import { LEAGUE_MATCH_PARTICIPANTS_TABLE } from "./schemas/MatchV5.js";
 
 export const db = drizzle({
 	connection: {
@@ -10,5 +12,22 @@ export const db = drizzle({
 	casing: "snake_case",
 });
 
-const task = new GameDataTask();
-//await task.updateEverything();
+async function insertMatch() {
+	const match = await riotHelper.getMatch("EUW1_7084514418");
+
+	if (match) {
+		const participants: (typeof LEAGUE_MATCH_PARTICIPANTS_TABLE.$inferInsert)[] =
+			match.info.participants.map((p) => {
+				return {
+					matchId: match.metadata.matchId,
+					queueId: match.info.queueId,
+					mapId: match.info.mapId,
+					gameMode: match.info.gameMode,
+					gameDuration: match.info.gameDuration,
+					...p,
+				};
+			});
+		await db.insert(LEAGUE_MATCH_PARTICIPANTS_TABLE).values(participants);
+	}
+	logger.debug("done");
+}
--- packages/server/src/db/schemas/MatchV5.ts
+++ packages/server/src/db/schemas/MatchV5.ts
@@ -7,6 +7,8 @@ import {
 	varchar,
 } from "drizzle-orm/pg-core";
 import { LEAGUE_CHAMPIONS_TABLE } from "./Champion.js";
+import { LEAGUE_GAME_MODES_TABLE } from "./GameMode.js";
+import { LEAGUE_MAPS_TABLE } from "./LeagueMap.js";
 import { LEAGUE_QUEUES_TABLE } from "./Queue.js";
 
 export const LEAGUE_MATCHES_TABLE = pgTable("league_matches", {
@@ -25,18 +27,23 @@ export const LEAGUE_MATCH_PARTICIPANTS_TABLE = pgTable(
 	"league_match_participants",
 	{
 		matchId: varchar().notNull(),
+		queueId: integer()
+			.notNull()
+			.references(() => LEAGUE_QUEUES_TABLE.queueId),
+		gameMode: varchar()
+			.notNull()
+			.references(() => LEAGUE_GAME_MODES_TABLE.gameMode),
+		mapId: integer()
+			.notNull()
+			.references(() => LEAGUE_MAPS_TABLE.mapId),
+		gameDuration: integer().notNull(),
+
 		puuid: varchar().notNull(),
 		championId: integer()
 			.notNull()
 			.references(() => LEAGUE_CHAMPIONS_TABLE.id),
-		queueId: integer()
-			.notNull()
-			.references(() => LEAGUE_QUEUES_TABLE.queueId),
-		//TODO gamemode
-		//TODO mapid
 		teamPosition: varchar().notNull(),
 		win: boolean().notNull(),
-		gameDuration: integer().notNull(),
 		kills: integer().notNull(),
 		deaths: integer().notNull(),
 		assists: integer().notNull(),
