c9e946dcd82fe2e13bc1fbc1b22148f2600709f1
AngryBacteria <AngryBacteria@protonmail.com>
2025-12-19 19:49:56 +0100

    remove unused code

--- packages/server/src/db/index.ts
+++ packages/server/src/db/index.ts
@@ -2,7 +2,6 @@ import { getColumns, type SQL, sql } from "drizzle-orm";
 import { drizzle } from "drizzle-orm/node-postgres";
 import type { PgTable } from "drizzle-orm/pg-core";
 import { DB_URL } from "../helpers/EnvironmentConfig.js";
-import { to } from "../helpers/General.js";
 import logger from "../helpers/Logger.js";
 import { dbRelations } from "./relations.js";
 import * as schema from "./schemas/index.js";
