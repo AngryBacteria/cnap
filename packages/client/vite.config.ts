be04b418bb22f0b091b4a244388cb24953b8a5fa
AngryBacteria <AngryBacteria@protonmail.com>
2026-05-14 15:26:41 +0200

    refactor: remove react compiler

--- packages/client/vite.config.ts
+++ packages/client/vite.config.ts
@@ -1,6 +1,6 @@
-import {defineConfig} from "vite";
+import { defineConfig } from "vite";
 import react from '@vitejs/plugin-react';
-import {tanstackRouter} from '@tanstack/router-plugin/vite'
+import { tanstackRouter } from '@tanstack/router-plugin/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -9,11 +9,7 @@ export default defineConfig({
             target: 'react',
             autoCodeSplitting: true,
         }),
-        react({
-            babel: {
-                plugins: ['babel-plugin-react-compiler'],
-            },
-        }),
+        react(),
     ],
     resolve: {
         alias: {
